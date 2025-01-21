e9b03e0e3c404056e6cc4a1003fcd2a7b93ebbdf
Adil Farooq <adilwarraixhh@gmail.com>
2025-01-21 15:22:03 +0500

    Add files via upload

---
+++ Final.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	//L1F24BSCS0132 {SOHAIB}
+	//L1F24BSCS0135 {Adil}
+	//L1F24BSCS0205 {FAHAD}
+	//L1F24BSCS0223 {SAAD}
+
+	/*
+	 Maam mera code Chat GPT se copied nahi hai !
+	*/
+
+	
+	//Declaring and Initialization of Name Array 
+	char name001[30] = {'\0'};
+	char name002[30] = {'\0'};
+	char name003[30] = {'\0'};
+	char name004[30] = {'\0'};
+	char name005[30] = {'\0'};
+	//Declaring and Initialization of Roll Number Array 
+	char roll001[20] = {'\0'};
+	char roll002[20] = {'\0'};
+	char roll003[20] = {'\0'};
+	char roll004[20] = {'\0'};
+	char roll005[20] = {'\0'};
+
+
+
+	int choice = 0;
+	cout << "1.  Enter Student Record" << endl;
+	cout << "2.  Display All Records" << endl;
+	cout << "3.  Update All Course Marks" << endl;
+	cout << "4.  Update Individual Course Marks" << endl;
+	cout << "5.  Update Roll Number" << endl;
+	cout << "6.  Delete Student Marks" << endl;
+	cout << "7.  Sort Records (by roll number)" << endl;
+	cout << "8.  Search Student Record (by roll number)" << endl;
+	cout << "9.  Calculate Grade For All Students" << endl;
+	cout << "10. Exit" << endl;
+
+	switch (choice)
+	{
+	case 1:
+
+		cout << "Enter Student Name: ";
+		cin >> name ;
+	}
+	
+
+
+
+
+	return 0;
+}
